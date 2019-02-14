1655a1aef8bd95396067ea958b6655acd7fdd102
Niels Koster <n.koster@portavita.eu>
2019-02-14 21:18:39 +0100

    WhyBot 0.001

--- whybot.js
+++ whybot.js
@@ -1,11 +1,11 @@
-weechat.register("test_js", "FlashCode", "1.0", "GPL3", "Test script", "", "");
+weechat.register("test_js", "WhyBot", "6.6.6", "GPL3", "WhyBot Script", "", "");
 
 function timer_cb(data, remaining_calls) {
-    weechat.print("", data);
+    weechat.print(weechat.current_buffer(), data);
     return weechat.WEECHAT_RC_OK;
 }
 
-weechat.hook_timer(5000, 0, 1, "timer_cb", "huuuuuu");
+weechat.hook_timer(5000, 0, 1, "timer_cb", "WhyBot");
 
 function vv_cb(data, buffer, args) {
     weechat.print(weechat.current_buffer(), "(-:\thello " + args)
@@ -20,12 +20,16 @@ function join_cb(data, signal, signal_data) {
 }
 weechat.hook_signal("*,irc_in2_join", "join_cb", "");
 
-weechat.hook_command('vv', 'WHOIS user', 'host', '', '', 'vv_cb', '');
-
 function priv_cb(data, signal, signal_data) {
     weechat.print(weechat.current_buffer(),"(-:\t" + signal + " " + signal_data);
     return weechat.WEECHAT_RC_OK
 }
 weechat.hook_signal("*,irc_in2_privmsg", "priv_cb", "");
 
-weechat.print("", "Hello, from javascript script!");
+function msg_cb(data, signal, signal_data) {
+    weechat.print(weechat.current_buffer(),"(o:\t" + signal + " " + signal_data);
+    return weechat.WEECHAT_RC_OK
+}
+weechat.hook_signal("*,irc_in2_msg", "msg_cb", "");
+
+weechat.print(weechat.current_buffer(), "WhyBot");
