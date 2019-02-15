ede085e4081cf5636b949a1cd2d4d87b5d8a731e
Niels Koster <n.koster@portavita.eu>
2019-02-15 22:46:25 +0100

    Use buffer "whybot" for output.

--- whybot.js
+++ whybot.js
@@ -1,4 +1,8 @@
-weechat.register("whybot", "WhyBot", "6.6.6", "GPL3", "WhyBot Script", "", "");
+weechat.register('whybot', 'WhyBot', '6.6.6', 'GPL3', 'WhyBot Script', '', '');
+
+var buffer = weechat.buffer_new('whybot', 'buffer_input_cb', '', 'buffer_close_cb', '');
+weechat.buffer_set(buffer, 'title', 'whybot data buffer');
+weechat.buffer_set(buffer, "localvar_set_no_log", "1");
 
 function timer_cb(data, remaining_calls) {
     weechat.print(weechat.current_buffer(), data);
@@ -11,11 +15,10 @@ function vv_cb(data, buffer, args) {
     weechat.print(weechat.current_buffer(), "(-:\thello " + args)
     return weechat.WEECHAT_RC_OK
 }
-
 weechat.hook_command('vv', 'WHOIS user', 'host', '', '', 'vv_cb', '');
 
 function join_cb(data, signal, signal_data) {
-    weechat.print(weechat.current_buffer(),"(-:\t" + signal + " " + signal_data);
+    weechat.print(buffer, "(-:\t" + signal + " " + signal_data);
     return weechat.WEECHAT_RC_OK
 }
 weechat.hook_signal("*,irc_in2_join", "join_cb", "");
@@ -25,14 +28,14 @@ function priv_cb(data, signal, signal_data) {
     var msg = sd.slice(3).toString();
     msg = msg.substr(1);
     if (sd[2][0] === '#') {
-        weechat.print(weechat.current_buffer(), '(-:\tchannel message');
+        weechat.print(buffer, '(-:\tchannel message');
     } else {
-        weechat.print(weechat.current_buffer(), '(-:\tprivate message from ' +
+        weechat.print(buffer, '(-:\tprivate message from ' +
             sd[2].substr(0, sd[2].length - 1) + ' ' + msg);
     }
-    weechat.print(weechat.current_buffer(),'(-:\t\"' + signal_data + '"');
+    weechat.print(buffer,'(-:\t\"' + signal_data + '"');
     return weechat.WEECHAT_RC_OK
 }
 weechat.hook_signal("*,irc_in2_privmsg", "priv_cb", "");
 
-weechat.print(weechat.current_buffer(), "WhyBot");
+weechat.print(weechat.current_buffer(), "WhyBotje");
