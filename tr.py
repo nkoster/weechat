907b55234c4089d15f529b2dae65873803fefc16
Niels Koster <niels.koster@portavita.nl>
2019-02-20 11:06:07 +0100

    Initial, Google Translate in Weechat.

---
+++ tr.py
@@ -0,0 +1,39 @@
+import weechat
+import urllib2
+
+weechat.register('translate', 'Translator', '0.0.1', 'GPL3', 'Google Translate Script', '', '')
+
+def timer_cb(data, remaining_calls):
+    weechat.prnt(weechat.current_buffer(), '%s' % data)
+    return weechat.WEECHAT_RC_OK
+
+weechat.hook_timer(2000, 0, 1, 'timer_cb', '/tr:\tUsing Google Translate')
+
+def tr_cb(data, buffer, args):
+    a = args.split(' ')
+    if len(a) < 2:
+        weechat.prnt(weechat.current_buffer(), '/tr:\tUsage /tr lang[,lang] text')
+        return weechat.WEECHAT_RC_OK
+    o = 'nl'
+    l = a[0]
+    ol = l.split(',')
+    tl = ol[0]
+    if len(ol) > 1:
+        o = ol[1]
+    t = ' '.join(a[1:])
+    url = 'https://translate.googleapis.com/translate_a/single' + \
+        '?client=gtx&sl=' + o + '&tl=' + tl + '&dt=t&q=' + t
+    url = url.replace(' ', '%20')
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0')
+    response = urllib2.urlopen(req)
+    html = response.read()
+    tr = html.split('"')[1]
+    if tr != 'nl':
+        if o == 'nl':
+            weechat.command(weechat.current_buffer(), '%s' % tr)
+        else:
+            weechat.prnt(weechat.current_buffer(), '/tr:\t%s' % tr)
+    return weechat.WEECHAT_RC_OK
+
+weechat.hook_command('tr', 'Translate', 'language and text', '', '', 'tr_cb', '')
