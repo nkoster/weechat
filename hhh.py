bfc26933ff0caead7acde41a89b19964401ee15b
Niels Koster <n.koster@portavita.eu>
2019-03-06 00:04:09 +0100

    Bla

--- hhh.py
+++ hhh.py
@@ -4,7 +4,17 @@ debug = True
 
 weechat.register('hal', 'hal9000', '6.6.6', 'GPL3', 'HAL Script', '', '')
 
-users = [ '*!*@82-197-212-247.dsl.cambrium.nl', '*!*80-101-145-252.ip.xs4all.nl' ]
+users = [
+    '*!*@82-197-212-247.dsl.cambrium.nl',
+    '*!*80-101-145-252.ip.xs4all.nl',
+    '*!*2a01:238:4350:ff00:c53e:c819:422a:2511'
+    ]
+
+def timer_cb(data, remaining_calls):
+    weechat.prnt(weechat.current_buffer(), '%s' % data)
+    return weechat.WEECHAT_RC_OK
+
+weechat.hook_timer(2000, 0, 1, 'timer_cb', 'HAL\tAll systems are operational.')
 
 def priv_cb(data, signal, signal_data):
 
@@ -19,12 +29,13 @@ def priv_cb(data, signal, signal_data):
     message = message[message.find(':') + 1:]
 
     if debug:
-        weechat.prnt(buffer, '-----\t---------------------------------')
+        weechat.prnt(buffer, '===\t========== Debug ==========')
         weechat.prnt(buffer, 'raw\t' + signal_data)
         weechat.prnt(buffer, 'vars\tnick=' + nick + ', user=' + user + ', host=' + host + ', target=' + target)
 
     if any(host in s for s in users):
         if message[0] == '/':
+            weechat.command(buffer, 'On behalf of ' + nick + ':')
             weechat.command(buffer, message)
         if debug:
             weechat.prnt(buffer, 'match\t' + host)
