310055bd9751f12eecbe929db320512f3bade9cb
Niels Koster <n.koster@portavita.eu>
2019-03-06 22:47:54 +0100

    Cleanup.

--- hhh.py
+++ hhh.py
@@ -2,8 +2,8 @@ import weechat
 import os.path
 import datetime
 
-userlist = 'userlist'
-debug = True
+userlist = './userlist'
+debug = False
 
 weechat.register('hal', 'hal9000', '6.6.6', 'GPL3', 'HAL Script', '', '')
 
@@ -15,23 +15,25 @@ def timer_cb(data, remaining_calls):
     global users
 
     if os.path.isfile(userlist):
-        weechat.prnt(current, 'HAL\tReading ' + userlist + '.')
+        weechat.prnt(current, 'HAL\tReading ' + userlist)
         file = open(userlist, 'r') 
         users = file.readlines()
     else:
-        weechat.prnt(current, "HAL\tuserlist doesn't exists.")
+        weechat.prnt(current, "HAL\tUsing default user list")
         users = [
             '*!*@82-197-212-247.dsl.cambrium.nl',
             '*!*@80-101-145-252.ip.xs4all.nl',
             '*!*@2a01:238:4350:ff00:c53e:c819:422a:2511'
         ]
+
     if debug:
         for u in users:
             weechat.prnt(current, 'user\t' + u)
+
     weechat.prnt(current, '%s' % data)
     return weechat.WEECHAT_RC_OK
 
-weechat.hook_timer(2000, 0, 1, 'timer_cb', 'HAL\tSystem fully operational.')
+weechat.hook_timer(2000, 0, 1, 'timer_cb', 'HAL\tSystem fully operational')
 
 def priv_cb(data, signal, signal_data):
 
@@ -50,9 +52,7 @@ def priv_cb(data, signal, signal_data):
         weechat.prnt(current, '===\t========== Debug ==========')
         weechat.prnt(current, 'raw\t' + signal_data)
         weechat.prnt(current, 'vars\tnick=' + nick + ', user=' + user + ', host=' + host + ', target=' + target)
-    if debug:
-        for u in users:
-            weechat.prnt(current, 'user\t' + u)
+
     if any(host in u for u in users):
         if message[0] == '/':
             if message[1:4] == 'say':
