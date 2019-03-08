1888ba5548cbaa5262c45ea6c2b5befbc23692ec
Niels Koster <n.koster@portavita.eu>
2019-03-08 21:16:34 +0100

    Cleanup, compress.

--- hhh.py
+++ hhh.py
@@ -5,6 +5,8 @@ import datetime
 
 debug = False
 greet = True
+userlist = '/home/niels/.weechat/userlist'
+users = []
 
 weechat.register('hal', 'The Hell', '6.6.6', 'GPL3', 'Hell Script', '', '')
 
@@ -19,15 +21,9 @@ def put(filename, users):
     file.writelines(users)
     file.close()
 
-userlist = '/home/niels/.weechat/userlist'
-
-users = []
-
 def timer_cb(data, remaining_calls):
-
     current = weechat.current_buffer()
     global users
-
     if os.path.isfile(userlist):
         weechat.prnt(current, 'HAL\tReading ' + userlist)
         users = get(userlist)
@@ -38,37 +34,29 @@ def timer_cb(data, remaining_calls):
             '*!*@80-101-145-252.ip.xs4all.nl',
             '*!*@2a01:238:4350:ff00:c53e:c819:422a:2511'
         ]
-
     if debug:
         for u in users:
             weechat.prnt(current, 'user\t' + u)
-
     weechat.prnt(current, '%s' % data)
     return weechat.WEECHAT_RC_OK
 
 weechat.hook_timer(2000, 0, 1, 'timer_cb', 'HAL\tSystem fully operational')
 
 def priv_cb(data, signal, signal_data):
-
     global users
-
     current = weechat.current_buffer()
-
     args = signal_data.split(' ')[0:3]
     nick = args[0][1:].split('!')[0]
     user = args[0][1:].split('!')[1].split('@')[0]
     host = args[0][1:].split('!')[1].split('@')[1]
     target = args[2]
     message = signal_data.split(' PRIVMSG ')[1].split(' :')[1]
-
     if debug:
         weechat.prnt(current, '===\t========== Debug ==========')
         weechat.prnt(current, 'raw\t' + signal_data)
         weechat.prnt(current, 'vars\tnick=' + nick + ', user=' + user + ', host=' + host + ', target=' + target)
         weechat.prnt(current, 'msg\t' + message)
-
     if any(host in u for u in users):
-
         if message[0] == '/':
             if len(message.split(' ')) > 1:
                 arg = message.split(' ')[1]
@@ -109,19 +97,15 @@ def priv_cb(data, signal, signal_data):
             if store:
                 weechat.prnt(current, 'HAL\tStoring ' + userlist)
                 put(userlist, users)
-
         if debug:
             for u in users:
                 weechat.prnt(current, 'user\t' + u)
-
         if debug:
             weechat.prnt(current, 'match\t' + host)
             weechat.prnt(current, 'msg\t' + message)
-    
     if 'HAL900' in message.upper():
         if 'HOE LAAT' in message.upper() or 'TIME' in message.upper():
             weechat.command(current, str(datetime.datetime.now()))
-    
     return weechat.WEECHAT_RC_OK
 
 weechat.hook_signal('*,irc_in2_privmsg', 'priv_cb', '')
